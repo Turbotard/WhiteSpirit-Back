c5edeac2694bdfb6eeb29720ae33f4d9fc27a465
Benjamin TIsserand <80269986+Turbotard@users.noreply.github.com>
2025-04-02 11:13:29 +0200

    Refactor ButtonHandler.js to simplify LED control by removing unused LED configurations and enhancing button press handling for toggling LED state with MQTT integration.

--- zigbee-node-central/src/utils/ButtonHandler.js
+++ zigbee-node-central/src/utils/ButtonHandler.js
@@ -3,8 +3,6 @@ const C = xbee_api.constants;
 
 // LED Control constants
 const LED_D1 = "D1";
-const LED_D2 = "D2";
-const LED_D3 = "D3";
 const LED_ON = "04";
 const LED_OFF = "00";
 
@@ -12,50 +10,42 @@ class ButtonHandler {
   constructor(xbeeAPI, mqttClient) {
     this.xbeeAPI = xbeeAPI;
     this.mqttClient = mqttClient;
-    this.currentLED = 0;
-    this.leds = [LED_D1, LED_D2, LED_D3];
+    this.isLedOn = false;
     
     // Button debounce
     this.lastButtonState = 1; // Initialize to 1 (not pressed) since button is pull-up
     this.buttonPressed = false;
-    this.buttonPressStartTime = 0;
-    this.longPressDelay = 1000; // 1 second for long press
 
-    // Configure LEDs on startup
-    this.configureLEDs();
+    // Configure LED on startup
+    this.configureLED();
   }
 
-  // Configure LEDs on startup
-  configureLEDs() {
-    console.log("Configuring LEDs...");
-    // Configure D1, D2, D3 as digital outputs
+  // Configure LED on startup
+  configureLED() {
+    console.log("Configuring LED D1...");
+    
+    // Configure D1 as digital output
     const configFrame = {
       type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
       destination64: "0013a20041fb6063",
-      command: "D1", // We'll configure each LED one by one
+      command: "D1",
       commandParameter: ["03"], // 03 = Digital Output
     };
     this.xbeeAPI.builder.write(configFrame);
     console.log("Sent D1 configuration");
 
-    // Configure D2
-    configFrame.command = "D2";
-    this.xbeeAPI.builder.write(configFrame);
-    console.log("Sent D2 configuration");
-
-    // Configure D3
-    configFrame.command = "D3";
-    this.xbeeAPI.builder.write(configFrame);
-    console.log("Sent D3 configuration");
+    // Turn off LED initially
+    this.controlLED(LED_OFF);
+    console.log("Turned off LED initially");
   }
 
   // Function to send command to control LED
-  controlLED(led, state) {
-    console.log(`Sending command: LED ${led} -> ${state}`);
+  controlLED(state) {
+    console.log(`Sending command: LED D1 -> ${state}`);
     const frame_obj = {
       type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
       destination64: "0013a20041fb6063",
-      command: led,
+      command: LED_D1,
       commandParameter: [state],
     };
     console.log("Frame object:", JSON.stringify(frame_obj, null, 2));
@@ -65,88 +55,32 @@ class ButtonHandler {
     // Publish LED state to MQTT
     if (this.mqttClient) {
       this.mqttClient.publish('zigbee/led/state', JSON.stringify({
-        led: led,
+        led: LED_D1,
         state: state === LED_ON ? 'on' : 'off'
-      }));
-    }
-  }
-
-  // Function to change to next LED
-  changeToNextLED() {
-    console.log(`Changing LED: Current LED is ${this.leds[this.currentLED]}`);
-    // Turn off current LED
-    this.controlLED(this.leds[this.currentLED], LED_OFF);
-    
-    // Move to next LED and turn it on
-    this.currentLED = (this.currentLED + 1) % this.leds.length;
-    console.log(`New LED will be ${this.leds[this.currentLED]}`);
-    this.controlLED(this.leds[this.currentLED], LED_ON);
-    
-    // Publish LED change to MQTT
-    if (this.mqttClient) {
-      this.mqttClient.publish('zigbee/led/current', JSON.stringify({
-        currentLED: this.currentLED,
-        ledName: this.leds[this.currentLED]
-      }));
-    }
-  }
-
-  // Function to turn off current LED
-  turnOffCurrentLED() {
-    console.log(`Turning off LED ${this.leds[this.currentLED]}`);
-    this.controlLED(this.leds[this.currentLED], LED_OFF);
-    
-    // Publish LED off to MQTT
-    if (this.mqttClient) {
-      this.mqttClient.publish('zigbee/led/current', JSON.stringify({
-        currentLED: this.currentLED,
-        ledName: this.leds[this.currentLED],
-        state: 'off'
       }));
     }
   }
 
   // Handle button state change
   handleButtonState(buttonState) {
-    console.log(`Button state: ${buttonState}, Last state: ${this.lastButtonState}, Button pressed: ${this.buttonPressed}`);
+    console.log(`Button state: ${buttonState}, Last state: ${this.lastButtonState}`);
     
-    // Detect button release (transition from 0 to 1)
-    if (buttonState === 1 && this.lastButtonState === 0) {
-      console.log("Button released - starting timer!");
-      this.buttonPressStartTime = Date.now();
-      this.buttonPressed = true;
-      
-      // Publish button release to MQTT
-      if (this.mqttClient) {
-        this.mqttClient.publish('zigbee/button/event', JSON.stringify({
-          event: 'released',
-          timestamp: Date.now()
-        }));
-      }
-    }
     // Detect button press (transition from 1 to 0)
-    else if (buttonState === 0 && this.lastButtonState === 1 && this.buttonPressed) {
-      const waitDuration = Date.now() - this.buttonPressStartTime;
-      console.log(`Button pressed after waiting ${waitDuration}ms`);
+    if (buttonState === 0 && this.lastButtonState === 1) {
+      console.log("Button pressed - toggling LED");
+      
+      // Toggle LED state
+      this.isLedOn = !this.isLedOn;
+      this.controlLED(this.isLedOn ? LED_ON : LED_OFF);
       
       // Publish button press to MQTT
       if (this.mqttClient) {
         this.mqttClient.publish('zigbee/button/event', JSON.stringify({
           event: 'pressed',
-          waitDuration: waitDuration,
-          isLongPress: waitDuration >= this.longPressDelay,
+          ledState: this.isLedOn ? 'on' : 'off',
           timestamp: Date.now()
         }));
       }
-      
-      if (waitDuration >= this.longPressDelay) {
-        console.log("Long wait detected - turning off LED");
-        this.turnOffCurrentLED();
-      } else {
-        console.log("Short wait detected - changing LED");
-        this.changeToNextLED();
-      }
-      this.buttonPressed = false;
     }
     
     this.lastButtonState = buttonState;
